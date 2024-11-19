313ecd5a10a5905a6ccb20c629a6b365c62f9a23
AnamAnsari <anama2379@gmail.com>
2024-11-19 20:35:02 +0530

    maani add new chnges plz work on it

---
+++ services/script.js
@@ -0,0 +1,5 @@
+// const container = document.querySelector(".flex-container");
+// container.addEventListener("wheel", (e) => {
+//   e.preventDefault();
+//   container.scrollLeft += e.deltaY; // Adjust scrolling speed
+// });
