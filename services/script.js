b6ad2a8af8665c8735f45fc1f79a98d682975e24
Muaaz Shaikh <mizerablepi@gmail.com>
2024-11-20 00:36:51 +0300

    basic

--- services/script.js
+++ services/script.js
@@ -1,5 +1,17 @@
-// const container = document.querySelector(".flex-container");
-// container.addEventListener("wheel", (e) => {
-//   e.preventDefault();
-//   container.scrollLeft += e.deltaY; // Adjust scrolling speed
-// });
+const slider = document.querySelector(".flex-container");
+let offset = 0;
+const sliderWidth = slider.scrollWidth;
+const containerWidth = slider.parentElement.offsetWidth;
+
+function slide() {
+  offset -= window.screen.width; // Adjust according to item width + gap
+
+  if (Math.abs(offset) >= sliderWidth) {
+    offset = 0;
+  }
+
+  slider.style.transform = `translateX(${offset}px)`;
+  console.log("SLIDe");
+}
+
+setInterval(slide, 2000); // Slide every 2 seconds
